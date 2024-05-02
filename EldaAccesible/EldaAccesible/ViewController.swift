9119e784cee113458e92a935d43743a40410600f
NahirRH <ronariher@hotmail.com>
2024-05-02 19:22:34 +0200

    add IBOutlets to ViewController

--- EldaAccesible/EldaAccesible/ViewController.swift
+++ EldaAccesible/EldaAccesible/ViewController.swift
@@ -6,14 +6,21 @@
 //
 
 import UIKit
+import MapKit
 
 class ViewController: UIViewController {
-
+    
+    @IBOutlet weak var mapView: MKMapView!
+    @IBOutlet weak var originTextField: UITextField!
+    @IBOutlet weak var destinationTextField: UITextField!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
     }
 
-
+    @IBAction func showRouteButton(_ sender: Any) {
+    }
+    
 }
 
