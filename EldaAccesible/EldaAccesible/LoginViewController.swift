3d71417f5001ee8c7d15e047066c5b35389e62b7
NahirRH <ronariher@hotmail.com>
2024-05-23 18:13:31 +0200

    create LoginViewController and add button functionality

---
+++ EldaAccesible/EldaAccesible/LoginViewController.swift
@@ -0,0 +1,23 @@
+//
+//  LoginViewController.swift
+//  EldaAccesible
+//
+//  Created by Manager on 23/5/24.
+//
+
+import UIKit
+
+class LoginViewController: UIViewController {
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+    }
+    
+    @IBAction func LoginButtonTouched(_ sender: Any) {
+        dismiss(animated: true)
+    }
+    
+    @IBAction func GuestButtonTouched(_ sender: Any) {
+        dismiss(animated: true)
+    }
+}
